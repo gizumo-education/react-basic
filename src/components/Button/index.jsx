a2af281718deb8c26137048329729e287cdfd941
村上　諒 <murakamiryo@murakamiryounoMacBook-Pro.local>
2024-12-18 10:42:39 +0900

    section4,index.jsxの記述修正

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,16 +1,7 @@
 import styles from './index.module.css'
 
-// export default function Button() {
-//   return (
-//     <button className={styles.button}>Button</button>
-//   )
-// }
-
-export const AlertButton = () => {
-const handleAlert = () => {
-  alert('ぽかーん')
-}
-return <button onClick={handleAlert} className={styles.button}>Alert</button>
+export default function Button() {
+  return (
+    <button className={styles.button}>Button</button>
+  )
 };
-
-export default AlertButton;
