96a23165a82e1a53b8bf9639562eb12363e4a3a3
unknown <s.tsuchida07@gmail.com>
2024-12-19 14:15:45 +0900

    section5練習問題

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -3,13 +3,16 @@ import styles from './index.module.css'
 export const Profile = () => {
   const imgUrl = 'https://i.imgur.com/7vQD0fPs.jpg';
   const name = 'John'
+  const birthday = 'Birthday:'
   const getBirthDay = () => {
     return new Date().toLocaleDateString()
   }
 
   return (
-    <img src='imgUrl' className={styles.avatar}>
-    <p className={styles.sentence}>I am John</p>
-    <p className={styles.sentence}>BirthDay: </p>
+    <>
+      <img src={imgUrl} className={styles.avatar}/>
+      <p className={styles.sentence}>{name}</p>
+      <p className={styles.sentence}>{birthday}{getBirthDay()}</p>
+    </>
   )
 }
