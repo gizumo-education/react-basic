b176464e3ea2e8a111e081e1bcf624cf01a9bb47
unknown <s.tsuchida07@gmail.com>
2024-12-20 14:16:22 +0900

    section11練習問題

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { TodoItem } from './components/TodoItem'
 import { LanguageList } from './components/LanguageList'
 import { AlertButton } from './components/AlertButton'
 import { InputField } from './components/InputField'
+import { CharacterImage } from './components/CharacterImage'
 
 export const App = () =>{
   return(
@@ -55,6 +56,9 @@ export const App = () =>{
 
       <hr />
       <InputField/>
+
+      <hr />
+      <CharacterImage/>
     </div>
   )
 }
--- src/components/CharacterImage/index.jsx
+++ src/components/CharacterImage/index.jsx
@@ -1,12 +1,14 @@
 import axios from 'axios'
 import { useState } from 'react'
+import { useEffect } from 'react'
 
 export const CharacterImage = () => {
   const [imgUrl, setImgUrl] = useState('')
 
-  axios.get('https://pokeapi.co/api/v2/pokemon/1').then((res) => {
+   useEffect(()=>{ axios.get('https://pokeapi.co/api/v2/pokemon/1').then((res) => {
+    // console.log(res.data.sprites.other['official-artwork']['front_default'])
     setImgUrl(res.data.sprites.other['official-artwork']['front_default'])
-  })
+  })}, [])
 
   return (
     <>
