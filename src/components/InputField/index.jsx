f347eccf175c1869ad660da752f8f7a844070d3c
unknown <s.tsuchida07@gmail.com>
2024-12-20 13:58:22 +0900

    section10練習問題

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,12 +1,27 @@
 import styles from './index.module.css'
+import { useState } from 'react'
+
+// export const InputField = () => {
+//   let value = '';
+
+//   return (
+//     <>
+//       <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
+//       <p>入力文字：{value}</p>
+//     </>
+//   )
+// }
 
 export const InputField = () => {
-  let value = ''
+  const [text, setText] = useState('')
+  const handelInput = (e) => {
+    setText(e.target.value)
+  }
 
   return (
     <>
-      <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
-      <p>入力文字：{value}</p>
+      <input onChange = {handelInput} className={styles.input} placeholder='任意の文字を入力' />
+      <p>入力文字：{text}</p>
     </>
   )
 }
