f997dfb026f102b12b99ce8b0530ebc7328d0d9b
Momoka Okuhisa <momokaokuhisa@MomokanoMacBook-Air.local>
2025-01-16 10:49:26 +0900

    feat: Country Cardを設置

--- src/components/Country/index.jsx
+++ src/components/Country/index.jsx
@@ -1,10 +1,29 @@
 import styles from './index.module.css'
+import PropTypes from 'prop-types'
 
-export const Country = () => {
+export const Country = ({ info, size }) => {
   return (
     <div>
-      <img src={info.imgUrl} width={width} height={height} />
+      <img src={info.imgUrl} width={size.width} height={size.height} />
       <p className={styles.name}>{info.name}</p>
     </div>
   )
 }
+
+Country.propTypes = {
+  info: PropTypes.exact({
+    imgUrl: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }),
+  size: PropTypes.exact({
+    width: PropTypes.number,
+    height: PropTypes.number,
+  }),
+};
+
+Country.defaultProps = {
+  size: {
+    width: 120,
+    height: 80,
+  },
+};
