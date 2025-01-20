5ebd5cb7f76a567259a2d57e2566c56e6660ae24
Momoka Okuhisa <momokaokuhisa@MomokanoMacBook-Air.local>
2025-01-20 11:25:23 +0900

    feat: InputFieldを設置

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,11 +1,12 @@
 import styles from './index.module.css'
+import { useState } from 'react'
 
 export const InputField = () => {
-  let value = ''
+  let [value, setValue] = useState('')
 
   return (
     <>
-      <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
+      <input onChange={(e) => value = setValue(e.target.value)} className={styles.input} placeholder='任意の文字を入力' />
       <p>入力文字：{value}</p>
     </>
   )
