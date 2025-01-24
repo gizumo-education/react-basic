3398ac6e1245fa10097a29fac5194b8ee9a28782
kaede suzuki <kaede072215@gmail.com>
2025-01-24 15:00:17 +0900

    section7_練習問題

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {Button} from './components/Button/index'
 import {Profile} from './components/Profile/index'
 import {Country} from './components/Country/index'
 import {Card} from './components/Card/index'
+import {TodoItem} from './components/TodoItem/index'
 
 export const App = () => {
   return(
@@ -30,8 +31,13 @@ export const App = () => {
       </Card>
 
       <hr/>
+      <ul>
+      <TodoItem name="Reactの学習" isToday={true} isCompleted={true} />
+      <TodoItem name="HTMLの復習" isToday={true} isCompleted={false} />
+      <TodoItem name="React Hooksの予習" isToday={false} isCompleted={false} />
+      </ul>
 
+      <hr/>
     </div>
   )
 }
-<hr />
--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -2,11 +2,15 @@ import PropTypes from 'prop-types'
 import styles from './index.module.css'
 
 export const TodoItem = ({ name, isToday, isCompleted }) => {
-  return <li className={styles.name}>{name}</li>
-}
+  return isToday ? ( //isTodayがtrueなら以下を処理
+    //↓<li>タグに.nameのクラスを付与して、{name}を表示。{isCompleted} が true なら ✔︎ を追加、false なら何も追加しない
+  <li className={styles.name}>{name} {isCompleted ? '✓' : ''}
+  </li>) : null; //isTodayがfalseなら何も表示しない(null)
+};
 
 TodoItem.propTypes = {
   name: PropTypes.string.isRequired,
   isToday: PropTypes.bool.isRequired,
   isCompleted: PropTypes.bool.isRequired,
 }
+
