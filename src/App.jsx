d1725a12fa30ca0a5734932e74b206ee42326da0
kaede suzuki <kaede072215@gmail.com>
2025-01-24 17:01:30 +0900

    section9_練習問題

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import {Profile} from './components/Profile/index'
 import {Country} from './components/Country/index'
 import {Card} from './components/Card/index'
 import {TodoItem} from './components/TodoItem/index'
+import {LanguageList} from './components/LanguageList/index'
+import { AlertButton } from './components/AlertButton/index'
 
 export const App = () => {
   return(
@@ -36,6 +38,12 @@ export const App = () => {
       <TodoItem name="HTMLの復習" isToday={true} isCompleted={false} />
       <TodoItem name="React Hooksの予習" isToday={false} isCompleted={false} />
       </ul>
+
+      <hr/>
+      <LanguageList/>
+
+      <hr/>
+      <AlertButton/>
 
       <hr/>
     </div>
--- src/components/LanguageList/index.jsx
+++ src/components/LanguageList/index.jsx
@@ -9,15 +9,15 @@ export const LanguageList = () => {
 
   return (
     <ul className={styles.wrapper}>
-      <li className={styles.item}>
+      <li key={languages.id} className={styles.item}>
         <img src={languages[0].imgUrl} />
         <p>{languages[0].name}</p>
       </li>
-      <li className={styles.item}>
+      <li key={languages.id} className={styles.item}>
         <img src={languages[1].imgUrl} />
         <p>{languages[1].name}</p>
       </li>
-      <li className={styles.item}>
+      <li key={languages.id} className={styles.item}>
         <img src={languages[2].imgUrl} />
         <p>{languages[2].name}</p>
       </li>
