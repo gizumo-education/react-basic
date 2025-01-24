0907483f314da087ee5941e6f6c2093eaf853d57
kaede suzuki <kaede072215@gmail.com>
2025-01-24 12:30:50 +0900

    section6_練習問題1,2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 import {Button} from './components/Button/index'
 import {Profile} from './components/Profile/index'
+import {Country} from './components/Country/index'
+import {Card} from './components/Card/index'
 
 export const App = () => {
   return(
@@ -18,6 +20,14 @@ export const App = () => {
 
       <hr/>
       <Profile/>
+
+      <hr/>
+      <Card>
+      <Country info = {{
+        imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+        name: 'アメリカ'
+      }}/>
+      </Card>
 
       <hr/>
 
