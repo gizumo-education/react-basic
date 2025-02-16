10ef4224c55876ab6595fbaa6abf16470c53b03a
natsukikokubu <natsukikokubu3@gmail.com>
2025-02-16 13:40:20 +0900

    Add: Buttonコンポーネントの表示

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import reactLogo from "./assets/react.svg";
 import styles from "./App.module.css";
+import { Button } from "./components/Button";
 
-// 以下に練習問題の回答を記述してください;
 export const App = () => {
   return (
     <div>
@@ -9,6 +9,10 @@ export const App = () => {
       <h1>Hello React!</h1>
 
       {/* 以下に練習問題の回答を記述してください */}
+      <hr />
+      <Button />
+      <Button />
+      <Button />
     </div>
   );
 };
