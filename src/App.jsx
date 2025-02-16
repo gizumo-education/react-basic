0d56a9fa4571af1d8bd68712cbdb692226250eca
natsukikokubu <natsukikokubu3@gmail.com>
2025-02-16 17:15:54 +0900

    Add: Profileコンポーネントを追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import reactLogo from "./assets/react.svg";
 import styles from "./App.module.css";
 import { Button } from "./components/Button";
+import { Profile } from "./components/Profile";
 
 export const App = () => {
   return (
@@ -13,6 +14,10 @@ export const App = () => {
       <Button />
       <Button />
       <Button />
+
+      <hr />
+      {/* <!-- Profileコンポーネントのタグをここに記述 --> */}
+      <Profile />
     </div>
   );
 };
