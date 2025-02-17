4c3f17c70d92111c7ac640aeeb23778851a65f5d
natsukikokubu <natsukikokubu3@gmail.com>
2025-02-17 17:15:16 +0900

    fix: BirthDayを修正しました

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -4,14 +4,14 @@ export const Profile = () => {
   const imgUrl = "https://i.imgur.com/7vQD0fPs.jpg";
   const name = "John";
   const getBirthDay = () => {
-    return new Date().toLocaleDateString();
+    return new Date("2023 / 7 / 7").toLocaleDateString();
   };
 
   return (
     <>
       <img src={imgUrl} className={styles.avatar} />
       <p className={styles.sentence}>I am {name}</p>
-      <p className={styles.sentence}>{getBirthDay()} </p>
+      <p className={styles.sentence}>BirthDay: {getBirthDay()} </p>
     </>
   );
 };
