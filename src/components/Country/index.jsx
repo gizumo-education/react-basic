ceebfaa8b768cd5cd398f62b67929927af8fc2f4
Takeru Kakuta <kakuta19960915@gmail.com>
2025-03-01 15:13:54 +0900

    セクション6の練習問題を記述

--- src/components/Country/index.jsx
+++ src/components/Country/index.jsx
@@ -1,10 +1,25 @@
-import styles from './index.module.css'
+import styles from "./index.module.css";
+import PropTypes from "prop-types";
 
-export const Country = () => {
+export const Country = ({ info, width, height }) => {
   return (
     <div>
       <img src={info.imgUrl} width={width} height={height} />
       <p className={styles.name}>{info.name}</p>
     </div>
-  )
-}
+  );
+};
+
+Country.propTypes = {
+  info: PropTypes.exact({
+    imgUrl: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+  width: PropTypes.number,
+  height: PropTypes.number,
+};
+
+Country.defaultProps = {
+  width: 120,
+  height: 80,
+};
