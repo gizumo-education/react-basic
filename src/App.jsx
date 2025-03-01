5f391f1a70e872ebcf3344348bd5ff673f9ce56d
Takeru Kakuta <kakuta19960915@gmail.com>
2025-03-01 16:25:22 +0900

    セクション8の練習問題の回答を記述

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Profile } from "./components/Profile";
 import { Country } from "./components/Country";
 import { Card } from "./components/Card";
 import { TodoItem } from "./components/TodoItem";
+import { LanguageList } from "./components/LanguageList";
 
 export const App = () => {
   return (
@@ -39,6 +40,8 @@ export const App = () => {
           isCompleted={false}
         />
       </ul>
+      <hr />
+      <LanguageList />
     </div>
   );
 };
