34a4472c3ef1a35f026ca1026184ef1ac6a1ee29
Takeru Kakuta <kakuta19960915@gmail.com>
2025-03-02 22:25:42 +0900

    セクション11の練習問題の回答を記述

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { TodoItem } from "./components/TodoItem";
 import { LanguageList } from "./components/LanguageList";
 import { AlertButton } from "./components/AlertButton";
 import { InputField } from "./components/InputField";
+import { CharacterImage } from "./components/CharacterImage";
 
 export const App = () => {
   return (
@@ -48,6 +49,8 @@ export const App = () => {
       <AlertButton />
       <hr />
       <InputField />
+      <hr />
+      <CharacterImage />
     </div>
   );
 };
--- src/components/CharacterImage/index.jsx
+++ src/components/CharacterImage/index.jsx
@@ -1,16 +1,12 @@
-import axios from 'axios'
-import { useState } from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export const CharacterImage = () => {
-  const [imgUrl, setImgUrl] = useState('')
-
-  axios.get('https://pokeapi.co/api/v2/pokemon/1').then((res) => {
-    setImgUrl(res.data.sprites.other['official-artwork']['front_default'])
-  })
-
-  return (
-    <>
-      {imgUrl && <img src={imgUrl} width={100} height={100} />}
-    </>
-  )
-}
+  const [imgUrl, setImgUrl] = useState("");
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon/1").then((res) => {
+      setImgUrl(res.data.sprites.other["official-artwork"]["front_default"]);
+    });
+  }, []);
+  return <>{imgUrl && <img src={imgUrl} width={100} height={100} />}</>;
+};
