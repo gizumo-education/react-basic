5d72dca1d9fa25b2a8607328856192d64475b5d0
haseyama <yamato@yamato.com>
2025-04-11 17:36:58 +0900

    props追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 import { Button } from './components/Button/index'
 import { Profile } from './components/Profile'
-
+import { Country } from './components/Country'
 
 export const App = () => {
   return (
@@ -19,6 +19,15 @@ export const App = () => {
       <Button />
       <hr />
       <Profile/>
+      <hr />
+      <Country
+        info={{
+          imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+          name: 'アメリカ',
+        }}
+        width={120}
+        height={80}
+      />
     </>
   )
 }
