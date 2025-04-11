fc676a282651a03c3fc91b6846980c367f19e3ec
taipeijapan <bumpofchelsea0910@gmail.com>
2025-04-11 10:40:39 +0900

    add:section4練習問題_ボタンを追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
+import Button from './components/Button/index'
 
 export const App = () => {
   return (
     <div>
       <img src={reactLogo} className={styles['react-logo']} />
       <h1>Hello React!</h1>
-
       {/* 以下に練習問題の回答を記述してください */}
+      <hr/>
+      <div class>
+        <Button />
+        <Button />
+        <Button />
+      </div>
     </div>
   )
 }
