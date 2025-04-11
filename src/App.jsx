2dc3e0f676e6fe0c3ab86308706b43f2d0dd42ad
haseyama <yamato@yamato.com>
2025-04-11 18:36:28 +0900

    Card Children(Country) 追加

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styles from './App.module.css'
 import { Button } from './components/Button/index'
 import { Profile } from './components/Profile'
 import { Country } from './components/Country'
+import { Card } from './components/Card'
 
 export const App = () => {
   return (
@@ -28,6 +29,17 @@ export const App = () => {
         width={120}
         height={80}
       />
+      <hr />
+      <Card>
+        <Country
+          info={{
+            imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+            name: 'アメリカ',
+          }}
+          width={120}
+          height={80}
+        />
+      </Card>
     </>
   )
 }
