eb2be27adde0bd6fc570261c5e916e14baa9d343
haseyama <yamato@yamato.com>
2025-04-11 16:21:34 +0900

    john profile追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 import { Button } from './components/Button/index'
+import { Profile } from './components/Profile'
+
 
 export const App = () => {
   return (
-    <div>
-      <img src={reactLogo} className={styles['react-logo']} />
+    <>
+      <img
+        src={reactLogo}
+        className={styles['react-logo']}
+      />
       <h1>Hello React!</h1>
       <hr />
       {/* 以下に練習問題の回答を記述してください */}
       <Button />
       <Button />
       <Button />
-    </div>
+      <hr />
+      <Profile/>
+    </>
   )
 }
