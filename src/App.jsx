c75d30cc8f1ffa43dd839f05e762a81e2ca8afff
haseyama <yamato@yamato.com>
2025-04-15 14:28:29 +0900

    alert追加

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Country } from './components/Country'
 import { Card } from './components/Card'
 import { TodoItem } from './components/TodoItem'
 import { LanguageList } from './components/LanguageList'
+import { AlertButton } from './components/AlertButton'
 
 export const App = () => {
   return (
@@ -50,6 +51,8 @@ export const App = () => {
       </ul>
       <hr />
       <LanguageList/>
+      <hr />
+      <AlertButton/>
     </>
   )
 }
--- src/components/AlertButton/index.jsx
+++ src/components/AlertButton/index.jsx
@@ -1,5 +1,9 @@
 import styles from './index.module.css'
 
 export const AlertButton = () => {
-  return <button className={styles.button}>Alert</button>
+  const Alert = () => {
+    alert('こんにちワンダフル、今日も1日頑張るニャン！')
+  }
+
+  return <button onClick={Alert} className={styles.button}>Alert</button>
 }
