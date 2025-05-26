48fc57f2d1b0129f9fd87d26e80d896d58b30929
高橋　輝 <hikaru0947@gmail.com>
2025-05-26 14:34:02 +0900

    Section11：useEffect練習問題1

--- src/components/CharacterImage/index.jsx
+++ src/components/CharacterImage/index.jsx
@@ -1,12 +1,15 @@
 import axios from 'axios'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export const CharacterImage = () => {
   const [imgUrl, setImgUrl] = useState('')
 
+useEffect(() => {
   axios.get('https://pokeapi.co/api/v2/pokemon/1').then((res) => {
     setImgUrl(res.data.sprites.other['official-artwork']['front_default'])
+    console.log(imgUrl);
   })
+}, [])
 
   return (
     <>
