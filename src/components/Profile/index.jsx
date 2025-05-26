f4f0e3bf765f92abf74e2470e419e50d11c4ea79
高橋　輝 <hikaru0947@gmail.com>
2025-05-26 14:03:53 +0900

    Section5：JSX構文練習問題1

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -2,14 +2,18 @@ import styles from './index.module.css'
 
 export const Profile = () => {
   const imgUrl = 'https://i.imgur.com/7vQD0fPs.jpg';
-  const name = 'John'
+  const name = '高橋輝';
   const getBirthDay = () => {
-    return new Date().toLocaleDateString()
-  }
+    return '1997/04/07';
+  };
 
   return (
-    <img src='imgUrl' className={styles.avatar}>
-    <p className={styles.sentence}>I am John</p>
-    <p className={styles.sentence}>BirthDay: </p>
-  )
-}
+    <div>
+      <img src={imgUrl} className={styles.avatar} alt={`${name}のプロフィール画像`} />
+      <p className={styles.sentence}>私は {name}です。</p>
+      <p className={styles.sentence}>BirthDay: {getBirthDay()}</p>
+    </div>
+  );
+};
+
+export default Profile;
