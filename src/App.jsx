68da823a80f91fe2df0be576a08f724ccd640771
omochi <omochi@example.com>
2025-06-18 13:55:17 +0900

    section4練習問題の回答

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
+import { Button } from './components/Button'
 
-
-   <div>
-      <img src={reactLogo} className={styles['react-logo']} />
-      <h1>Hello React!</h1>
-   </div>
       {/* 以下に練習問題の回答を記述してください */}
-      
-   
-
       
 export const App = () => {
    return (
       <div>
          <img src={reactLogo} className={styles['react-logo']} />
          <h1>Hello React!</h1>
+
+         <hr />
+         <div style={{ display: 'flex'}}>
+         <Button /><Button /><Button />
+         </div>
+      
+
       </div>
    )
 }
-
-
 
