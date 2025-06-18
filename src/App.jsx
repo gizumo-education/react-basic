7b9c0c4ecc058bb0b5cc7f8aa8b974efe7c3ab9d
omochi <omochi@example.com>
2025-06-18 11:19:26 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,26 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
+
+
+   <div>
+      <img src={reactLogo} className={styles['react-logo']} />
+      <h1>Hello React!</h1>
+   </div>
+      {/* 以下に練習問題の回答を記述してください */}
+      
+
+      
+export const App = () => {
+   return (
+      <div>
+         <img src={reactLogo} className={styles['react-logo']} />
+         <h1>Hello React!</h1>
+      </div>
+   )
+}
+
+   
+
+   
+
+
