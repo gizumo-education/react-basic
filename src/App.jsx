216e2a264d7569279c335834163712d39ad7aa1a
omochi <omochi@example.com>
2025-06-19 00:33:23 +0900

    section6練習問題の回答

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 import { Button } from './components/Button'
 import { Profile } from './components/Profile'
+import { Country } from './components/Country'
+import {Card} from './components/Card'
 
       {/* 以下に練習問題の回答を記述してください */}
       
@@ -21,6 +23,19 @@ export const App = () => {
          <hr /> 
             < Profile />
          
+         <hr />
+         
+            <Card>
+            <Country
+               imgInfo={{
+                  imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+                  name: 'アメリカ'
+               }}
+            />
+            </Card>
+            
+         
+
          
 
       </div>
