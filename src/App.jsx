167dc8bb65ac4d56ee7e5810d1187f9fbe213777
omochi <omochi@example.com>
2025-06-19 10:38:12 +0900

    section7練習問題の回答

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Button } from './components/Button'
 import { Profile } from './components/Profile'
 import { Country } from './components/Country'
 import {Card} from './components/Card'
+import {TodoItem} from './components/TodoItem'
 
       {/* 以下に練習問題の回答を記述してください */}
       
@@ -33,7 +34,14 @@ export const App = () => {
                }}
             />
             </Card>
-            
+
+            <hr />
+            <ul>
+               <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
+               <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
+               <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
+            </ul>
+                        
          
 
          
