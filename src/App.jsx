49d845eaac2ac2ab0c32a74f8d20f3940009ee9f
omochi <omochi@example.com>
2025-06-19 12:31:21 +0900

    section9練習問題の回答

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Country } from './components/Country'
 import {Card} from './components/Card'
 import {TodoItem} from './components/TodoItem'
 import {LanguageList} from './components/LanguageList'
+import { AlertButton } from './components/AlertButton'
 
       {/* 以下に練習問題の回答を記述してください */}
       
@@ -44,6 +45,10 @@ export const App = () => {
 
             <hr />
             <LanguageList />
+
+            <hr />
+            <AlertButton />
+
 
 
 
