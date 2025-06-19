20bbbdc861071d637c55e15212ad8b3b95c24334
omochi <omochi@example.com>
2025-06-19 17:27:47 +0900

    section11練習問題の回答

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import {TodoItem} from './components/TodoItem'
 import {LanguageList} from './components/LanguageList'
 import { AlertButton } from './components/AlertButton'
 import { InputField } from './components/InputField'
-
+import { CharacterImage } from './components/CharacterImage'
       {/* 以下に練習問題の回答を記述してください */}
       
 export const App = () => {
@@ -53,6 +53,8 @@ export const App = () => {
             <hr />
             <InputField  />
 
+            <hr />
+            <CharacterImage />
 
 
 
