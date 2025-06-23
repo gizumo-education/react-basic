fed0cfd8e9e636d2e11115a351088d3cc5e6fbd5
Hyogo03 <sasahyogo@gmail.com>
2025-06-23 22:14:48 +0900

    John画像生成

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 import { Button } from './components/Button';
+import { Profile } from './components/Profile';
 
 export const App = () => {
   return (
@@ -9,11 +10,16 @@ export const App = () => {
       <h1>Hello React!</h1>
 
       {/* 以下に練習問題の解答を記述してください */}
+      
       <hr />
       {/* <!-- Buttonコンポーネントのタグをここに3つ記述 --> */}
       <Button />
       <Button />
       <Button />
+
+      <hr />
+      {/* <!-- Profileコンポーネントのタグをここに記述 --> */}
+      <Profile />
     </div>
   )
 };
