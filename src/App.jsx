f113d019c1aa462360913c190ca7971c6da29e50
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2025-06-24 16:18:43 +0900

    add:TodoItemコンポーネントの実装と使用

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {Button} from './components/Button'
 import {Profile} from './components/Profile'
 import {Country} from './components/Country'
 import {Card} from './components/Card'
+import {TodoItem} from './components/TodoItem'
 
 export const App = () => {
   return (
@@ -35,6 +36,12 @@ export const App = () => {
           }}
         />
       </Card>
+      <hr />
+      <ul>
+        <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
+        <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
+        <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
+      </ul>
     </div>
   );
 };
--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -2,8 +2,11 @@ import PropTypes from 'prop-types'
 import styles from './index.module.css'
 
 export const TodoItem = ({ name, isToday, isCompleted }) => {
-  return <li className={styles.name}>{name}</li>
-}
+  return (isToday && (
+    <li className={styles.name}>{name}{isCompleted ? '✅':''} </ li>
+  ));
+};
+
 
 TodoItem.propTypes = {
   name: PropTypes.string.isRequired,
