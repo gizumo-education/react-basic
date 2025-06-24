aada2bb8bb94072598e217950c0a4b00cac5eb4d
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2025-06-24 14:28:23 +0900

    add:Countryコンポーネントを定義し、使用

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 import {Button} from './components/Button'
 import {Profile} from './components/Profile'
+import {Country} from './components/Country'
 
 export const App = () => {
   return (
@@ -16,6 +17,15 @@ export const App = () => {
       <Button />
       <hr />
       <Profile />
+      <hr />
+      <Country
+        info = {{
+            imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+            name: 'アメリカ'
+        }}
+        width = {120}
+        height = {80}
+      />
     </div>
   );
 };
