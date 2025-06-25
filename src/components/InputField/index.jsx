f832e95a53b13624e67c18988b1584f664abd28b
Hyogo03 <sasahyogo@gmail.com>
2025-06-26 00:54:55 +0900

    10. フックとuseState

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,12 +1,25 @@
+import { useState } from 'react'
 import styles from './index.module.css'
 
 export const InputField = () => {
-  let value = ''
 
+  const [value, setValue] = useState('');
+  const handleStr = (e) => {
+    setValue(e.target.value);
+  }
   return (
     <>
-      <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
+      <input onChange={handleStr} className={styles.input} placeholder='任意の文字を入力' />
       <p>入力文字：{value}</p>
     </>
   )
+
+  // let value = ''
+
+  // return (
+  //   <>
+  //     <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
+  //     <p>入力文字：{value}</p>
+  //   </>
+  // )
 }
