3d9e4837d619e5850b8616bb1560a0d0abfd9d34
Naoto0211TT <naozaki334@gmail.com>
2025-06-30 00:49:06 +0900

    'ToDoの追加'

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -2,8 +2,9 @@ import PropTypes from 'prop-types'
 import styles from './index.module.css'
 
 export const TodoItem = ({ name, isToday, isCompleted }) => {
-  return <li className={styles.name}>{name}</li>
-}
+  if (!isToday) return null;
+  return <li className={styles.name}>{isCompleted ? name + ' ✔︎' : name}</li>;
+};
 
 TodoItem.propTypes = {
   name: PropTypes.string.isRequired,
