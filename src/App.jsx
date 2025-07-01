793648d6b45f713a87ffb3d57bec075aa1e0c96e
Naoto0211TT <naozaki334@gmail.com>
2025-07-01 23:20:55 +0900

    'ボタンの追加'

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Country } from './components/Country'
 import { Card } from './components/Card'
 import { TodoItem } from './components/TodoItem'
 import { LanguageList } from './components/LanguageList'
+import { AlertButton } from './components/AlertButton'
 
 export const App = () => {
   return (
@@ -33,6 +34,7 @@ export const App = () => {
             <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
           </ul>
           <hr /><LanguageList />
+          <hr /><AlertButton />
         </>
       }
     </div>
