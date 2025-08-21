6b01d2dcb6880be2ed0328d3c650cfb2932c2b76
濵田沙羅 <sarada20020628@gmail.com>
2025-08-21 15:02:45 +0900

    section4の練習問題を記入

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,14 @@
 import styles from './index.module.css'
 
-export default function Button() {
+// export default function Button() {
+//   return (
+//     <button className={styles.button}>Button</button>
+//   )
+// }
+
+// named exportに変更
+export function Button() {
   return (
     <button className={styles.button}>Button</button>
-  )
+  );
 }
