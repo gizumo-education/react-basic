c90a8d9b3f1c82df3bb899eea2e4c4a23b52cf20
濵田沙羅 <sarada20020628@gmail.com>
2025-08-22 17:14:09 +0900

    section9の練習問題を記入

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Country } from './components/Country';
 import { Card } from './components/Card';
 import { TodoItem } from './components/TodoItem';
 import { LanguageList } from './components/LanguageList';
+import { AlertButton } from './components/AlertButton';
 
 export const App = () => {
     return (
@@ -56,6 +57,10 @@ export const App = () => {
         <hr/>
         {/* section8  リストレンダー */}
         <LanguageList/>
+
+        <hr/>
+        {/* section9  イベントハンドラ */}
+        <AlertButton/>
         </div>
     );
 };
