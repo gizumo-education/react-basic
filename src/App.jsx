0b2623c45b4b021bd78ed4df3c5de6fa631c45fa
濵田沙羅 <sarada20020628@gmail.com>
2025-08-22 15:27:47 +0900

    section7の練習問題を記入

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,17 @@ import { Button } from './components/Button';
 import { Profile } from './components/Profile';
 import { Country } from './components/Country';
 import { Card } from './components/Card';
+import { TodoItem } from './components/TodoItem';
 
 export const App = () => {
     return (
         <div>
-         <img src={reactLogo} alt="" className={styles['react-logo']}/>
-         <h1>Hello React!</h1>
+        <img src={reactLogo} alt="" className={styles['react-logo']}/>
+        <h1>Hello React!</h1>
 
          {/* 以下に練習問題の回答をしてください */}
 
-         <hr />
+        <hr />
          {/* section4 Buttonのコンポーネントタグを３つ追加 */}
         <Button />
         <Button />
@@ -42,6 +43,14 @@ export const App = () => {
                 }}
             />
         </Card>
+
+        <hr/>
+        {/* section7 練習問題2 条件付きレンダー */}
+        <ul>
+            <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
+            <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
+            <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
+        </ul>
 
         </div>
     );
