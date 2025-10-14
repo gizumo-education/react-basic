86571b78753e32e7861f1ca0bda013190ce69b12
janjan519 <207494690+janjan519@users.noreply.github.com>
2025-10-14 17:34:25 +0900

    add:section5 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import reactLogo from "./assets/react.svg";
 import styles from "./App.module.css";
 import Button from "./components/Button";
+import { Profile } from "./components/Profile";
 
 export const App = () => {
   return (
@@ -8,11 +9,13 @@ export const App = () => {
       <img src={reactLogo} className={styles["react-logo"]} />
       <h1>Hello React!</h1>
 
-      {/* 以下に練習問題の回答を記述してください */}
       <hr />
       <Button />
       <Button />
       <Button />
+
+      <hr />
+      <Profile />
     </div>
   );
 };
--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,15 +1,17 @@
-import styles from './index.module.css'
+import styles from "./index.module.css";
 
 export const Profile = () => {
-  const imgUrl = 'https://i.imgur.com/7vQD0fPs.jpg';
-  const name = 'John'
+  const imgUrl = "https://i.imgur.com/7vQD0fPs.jpg";
+  const name = "John";
   const getBirthDay = () => {
-    return new Date().toLocaleDateString()
-  }
+    return new Date().toLocaleDateString();
+  };
 
   return (
-    <img src='imgUrl' className={styles.avatar}>
-    <p className={styles.sentence}>I am John</p>
-    <p className={styles.sentence}>BirthDay: </p>
-  )
-}
+    <>
+      <img src={imgUrl} className={styles.avatar} />
+      <p className={styles.sentence}>I am {name}</p>
+      <p className={styles.sentence}>BirthDay: {getBirthDay()}</p>
+    </>
+  );
+};
