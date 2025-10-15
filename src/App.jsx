5150e0705154338d36addedc966b87974bc83669
takahashi tomasu <takahashitomasu@gmail.com>
2025-10-15 15:45:50 +0900

    セクション３〜４

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,20 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
+import Button from './components/Button'
+
+export function App() {
+  return (
+    <div>
+      <img src={reactLogo} className={styles['react-logo']} />
+      <h1>Hello React!</h1>
+
+      <hr />
+    {/* Buttonコンポーネントのタグをここに3つ記述 */}
+      <Button />
+        <Button />
+        <Button />
+      
+    </div>
+  )
+
+} 
