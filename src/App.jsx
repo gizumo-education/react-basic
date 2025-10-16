0b7c27674eb899f7b9d159923b80dfcd7aafadef
takahashi tomasu <takahashitomasu@gmail.com>
2025-10-16 12:20:11 +0900

    セクション11

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import Button from './components/Button'
 import { Profile } from './components/Profile'
 import { Country } from "./components/Country";
 import { Card } from "./components/Card";
+import { TodoItem } from "./components/TodoItem";
+import { LanguageList } from './components/LanguageList'
+import { AlertButton } from './components/AlertButton';
+import { InputField } from './components/InputField';
+import { CharacterImage } from './components/CharacterImage';
 
 export const App =() => {
   return (
@@ -11,18 +16,18 @@ export const App =() => {
       <img src={reactLogo} className={styles['react-logo']} />
       <h1>Hello React!</h1>
 
-      <hr />
-    {/* Buttonコンポーネントのタグをここに3つ記述 */}
+    <hr />
+    {/* Section 4: Buttonコンポーネントのタグをここに3つ記述 */}
       <Button />
       <Button />
       <Button />
 
-      <hr />
-    {/* Profileコンポーネントのタグをここに記述 */}
+    <hr />
+    {/* Section 5: Profileコンポーネントのタグをここに記述 */}
       <Profile />
 
-      <hr />
-    {/* CardコンポーネントとCountryコンポーネントのタグをここに記述 */}
+    <hr />
+    {/* Section 6: CardコンポーネントとCountryコンポーネントのタグをここに記述 */}
       <Card>
         <Country
           info={{
@@ -32,7 +37,29 @@ export const App =() => {
         />
       </Card>
 
-      <hr />
+    <hr />
+    {/* Section 7: ulタグとTodoItemコンポーネントのタグをここに記述 */}
+    <ul>
+      <TodoItem name="Reactの学習" isToday={true} isCompleted={true} />
+      <TodoItem name="HTMLの復習" isToday={true} isCompleted={false} />
+      <TodoItem name="React Hooksの予習" isToday={false} isCompleted={false} />
+    </ul>
+
+    <hr />
+    {/* Section 8: LanguageListコンポーネントのタグをここに記述 */}
+    <LanguageList />
+
+    <hr />
+    {/* Section 9: AlertButtonコンポーネントのタグをここに記述 */}
+    <AlertButton />
+
+    <hr />
+    {/* Section 10: InputFieldコンポーネントのタグをここに記述 */}
+    <InputField />
+
+    <hr />
+    {/* Section 11: CharacterImageコンポーネントのタグをここに記述 */}
+    <CharacterImage />
       
     </div>
   )
