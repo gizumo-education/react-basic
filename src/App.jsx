24189e850a2e5ee4dc7a59901bc290f4de96654c
janjan519 <207494690+janjan519@users.noreply.github.com>
2025-10-16 11:47:10 +0900

    add:section11 done

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { TodoItem } from "./components/TodoItem";
 import { LanguageList } from "./components/LanguageList";
 import { AlertButton } from "./components/AlertButton";
 import { InputField } from "./components/InputField";
+import { CharacterImage } from "./components/CharacterImage";
 
 export const App = () => {
   return (
@@ -54,6 +55,9 @@ export const App = () => {
 
       <hr />
       <InputField />
+
+      <hr />
+      <CharacterImage />
     </div>
   );
 };
