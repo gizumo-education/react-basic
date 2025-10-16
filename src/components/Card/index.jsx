1e31e790f43125506b479abb91e98d85116958cf
takahashi tomasu <takahashitomasu@gmail.com>
2025-10-16 10:58:29 +0900

    セクション６

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,16 +1,14 @@
-import PropTypes from 'prop-types'
-import styles from './index.module.css'
+import PropTypes from "prop-types";
+import styles from "./index.module.css";
 
-export const Card = () => {
-  return (
-    <div className={styles.card}></div>
-  )
-}
+export const Card = ({ children }) => {
+  return <div className={styles.card}>{children}</div>;
+};
 
 Card.propTypes = {
-  children: PropTypes.node
-}
+  children: PropTypes.node,
+};
 
 Card.defaultProps = {
-  children: null
-}
+  children: null,
+};
