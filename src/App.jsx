dd29271401439fad18b6583b1077e9779a35c7e9
yuuna <yuuna@yuunanoMacBook-Pro.local>
2025-11-18 18:38:27 +0900

    push:section6の回答を記載

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 import { Button } from './components/Button/index.jsx'
 import { Profile } from './components/Profile/index.jsx'
+import { Country } from './components/Country/index.jsx'
+import { Card } from './components/Card/index.jsx'
 
 export const App = () => {
   return (
@@ -13,14 +15,37 @@ export const App = () => {
 
     <hr />
     {/* <!-- Buttonコンポーネントのタグをここに3つ記述 --> */}
-    <div>
+    <>
       <Button />
       <Button />
       <Button />
-    </div>
+    </>
     <hr />
+    {/* <!-- Profileコンポーネントのタグをここに記述 --> */}
     <>
       <Profile />
+    </>
+    <hr />
+    {/* <!-- Countryコンポーネントのタグをここに記述 --> */}
+    <>
+     <Country 
+      info={{
+        imgUrl:'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+        name: 'アメリカ'
+      }}
+     />
+    <hr />
+    {/* <!-- CardコンポーネントとCountryコンポーネントのタグをここに記述 --> */}
+    <>
+      <Card>
+        <Country
+          info={{
+            imgUrl:'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+            name: 'アメリカ'
+          }}
+        />
+      </Card>
+    </>
     </>
   </>
   );
--- src/components/Country/index.jsx
+++ src/components/Country/index.jsx
@@ -1,10 +1,27 @@
+import PropTypes from 'prop-types'
 import styles from './index.module.css'
 
-export const Country = () => {
+export const Country = ({ info , width , height }) => {
   return (
     <div>
       <img src={info.imgUrl} width={width} height={height} />
       <p className={styles.name}>{info.name}</p>
     </div>
   )
+}
+
+// デフォルト値を設定
+Country.defaultProps = {
+  width: 120,
+  height: 60
+}
+
+// propsの型を定義する
+Country.propTypes = {
+  info: PropTypes.exact({
+    imgUrl: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired
+  }),
+  width: PropTypes.number,
+  height: PropTypes.number,
 }
