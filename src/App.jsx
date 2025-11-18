e443a67350c0fab472397a123539b53288fc4699
yuuna <yuuna@yuunanoMacBook-Pro.local>
2025-11-18 16:19:46 +0900

    push:section4と5の回答を記載

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
+import { Button } from './components/Button/index.jsx'
+import { Profile } from './components/Profile/index.jsx'
 
 export const App = () => {
   return (
-    <div>
+    <>
     <img src={reactLogo} className={styles['react-logo']} />
     <h1>Hello React!</h1>
 
     {/* 以下に練習問題の回答を記述してください */}
-  </div>
+
+    <hr />
+    {/* <!-- Buttonコンポーネントのタグをここに3つ記述 --> */}
+    <div>
+      <Button />
+      <Button />
+      <Button />
+    </div>
+    <hr />
+    <>
+      <Profile />
+    </>
+  </>
   );
 };
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,13 @@
 import styles from './index.module.css'
 
-export default function Button() {
+// export default function Button() {
+//   return (
+//     <button className={styles.button}>Button</button>
+//   )
+// }
+
+export const Button = () => {
   return (
     <button className={styles.button}>Button</button>
-  )
-}
+  );
+};
