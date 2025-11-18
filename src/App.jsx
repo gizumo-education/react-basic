f2d99c904e197cb36859af2f47fa5eefd347f32b
新井　路花 <arahana@araijihananoMacBook-Air.local>
2025-11-18 17:19:55 +0900

    section6 練習問題の回答

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 import {Button} from './components/Button'
 import {Profile} from './components/Profile'
+import {Country} from './components/Country'
+import {Card} from './components/Card'
+
 
 export const App = () => {
   return (
@@ -19,6 +22,18 @@ export const App = () => {
 
       <hr />
       <Profile />
+
+      <hr />
+      {/* 子コンポーネントにpropsを渡す */}
+      {/* children propsを受け取れるように入れ子にする */}
+      <Card >
+        <Country 
+          info={{
+            imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+            name: 'アメリカ'
+          }} 
+        />
+      </Card >
 
 
 
--- src/components/Country/index.jsx
+++ src/components/Country/index.jsx
@@ -1,10 +1,30 @@
 import styles from './index.module.css'
+import PropTypes from 'prop-types'
 
-export const Country = () => {
+// propsを受け取れるようにする
+export const Country = ( { info, width, height }) => {
   return (
     <div>
       <img src={info.imgUrl} width={width} height={height} />
       <p className={styles.name}>{info.name}</p>
     </div>
   )
+}
+
+
+//propsの型の定義
+Country.propTypes = {
+  info: PropTypes.exact({
+    imgUrl:PropTypes.string.isRequired,
+    name:PropTypes.string.isRequired
+  }).isRequired,
+  width: PropTypes.number,
+  height: PropTypes.number
+}
+
+
+//propsのデフォルト値を設定
+Country.defaultProps = {
+  width: 120,
+  height: 80
 }
