e2fa2d7001f25e1f76a0a9ca0de32b95708d421a
yuuna <yuuna@yuunanoMacBook-Pro.local>
2025-11-19 15:55:11 +0900

    push:section11の回答記載

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { TodoItem } from './components/TodoItem/index.jsx'
 import { LanguageList } from './components/LanguageList/index.jsx'
 import { AlertButton } from './components/AlertButton/index.jsx'
 import { InputField } from './components/InputField/index.jsx'
+import { CharacterImage } from './components/CharacterImage/index.jsx'
 
 export const App = () => {
   return (
@@ -73,6 +74,11 @@ export const App = () => {
     {/* <!-- InputFieldコンポーネントのタグをここに記述 --> */}
     <>
       <InputField />
+    </>
+    <hr />
+    {/* <!-- CharacterImageコンポーネントのタグをここに記述 --> */}
+    <>
+      <CharacterImage />
     </>
     </>
     </>
