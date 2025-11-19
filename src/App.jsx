3083630c2b3a56dfe895b49aff4d6e7f9330b181
yuuna <yuuna@yuunanoMacBook-Pro.local>
2025-11-19 15:00:32 +0900

    section10の回答記載

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Card } from './components/Card/index.jsx'
 import { TodoItem } from './components/TodoItem/index.jsx'
 import { LanguageList } from './components/LanguageList/index.jsx'
 import { AlertButton } from './components/AlertButton/index.jsx'
+import { InputField } from './components/InputField/index.jsx'
 
 export const App = () => {
   return (
@@ -67,6 +68,11 @@ export const App = () => {
     {/* <!-- AlertButtonコンポーネントのタグをここに記述 --> */}
     <>
     <AlertButton />
+    </>
+    <hr />
+    {/* <!-- InputFieldコンポーネントのタグをここに記述 --> */}
+    <>
+      <InputField />
     </>
     </>
     </>
--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,11 +1,13 @@
 import styles from './index.module.css'
+import { useState } from 'react'
 
 export const InputField = () => {
-  let value = ''
+  const [value, setValue] = useState('')
 
   return (
     <>
-      <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
+    {/* onChangeイベントで入力ごとに発火 */}
+      <input onChange={(e) => value = setValue(e.target.value)} className={styles.input} placeholder='任意の文字を入力' />
       <p>入力文字：{value}</p>
     </>
   )
