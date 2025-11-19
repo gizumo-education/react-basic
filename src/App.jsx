dbdf5da5d2430b55d1d21cd0fcfeb668e7e27492
新井　路花 <arahana@araijihananoMacBook-Air.local>
2025-11-19 12:09:51 +0900

    section10 練習問題useStateの回答

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {Card} from './components/Card'
 import {TodoItem} from './components/TodoItem'
 import {LanguageList} from './components/LanguageList'
 import {AlertButton} from './components/AlertButton'
+import {InputField} from './components/InputField'
 
 
 
@@ -57,6 +58,11 @@ export const App = () => {
       <hr />
       {/* section9 イベントハンドラ */}
       <AlertButton />
+
+
+      <hr />
+      {/* section10 フック */}
+      <InputField />
 
 
 
--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,12 +1,16 @@
 import styles from './index.module.css'
+import { useState } from 'react'
 
 export const InputField = () => {
-  let value = ''
+  const [value, setText] = useState('')
+  const handleChange = (e) => {
+    setText(e.target.value)
+  }
 
   return (
     <>
-      <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
-      <p>入力文字：{value}</p>
+      <input onChange={ handleChange } className={styles.input} placeholder='任意の文字を入力'/>
+      <p>入力文字：{ value }</p>
     </>
   )
 }
