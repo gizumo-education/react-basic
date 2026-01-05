3b6a2033fd6f8e99d6024ed3cc506acbb2944dde
海老名彩希 <ebinamasato@MacBook-Air.local>
2026-01-05 18:55:23 +0900

    問題１についての回答

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,13 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
+
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>
+      <img src={reactLogo} className={styles['react-logo']} />
+      <h1>Hello React!</h1>
+    </div>
+  )
+}
