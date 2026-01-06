7e1bf892a1e00354a17b6614c0523a961a9745be
海老名彩希 <ebinamasato@MacBook-Air.local>
2026-01-06 18:52:25 +0900

    section8練習問題

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Profile } from './components/Profile'
 import { Country }  from './components/Country'
 import { Card } from './components/Card'
 import { TodoItem } from './components/TodoItem'
+import { LanguageList } from './components/LanguageList'
 
 // import React from 'react'
 
@@ -33,12 +34,16 @@ export const App = () => {
       </Card>
 
       <hr />
-      
+
       <ul>
         <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
         <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
         <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
       </ul>
+
+      <hr />
+
+      <LanguageList />
 
     </div>
   )
--- src/components/LanguageList/index.jsx
+++ src/components/LanguageList/index.jsx
@@ -9,18 +9,15 @@ export const LanguageList = () => {
 
   return (
     <ul className={styles.wrapper}>
-      <li className={styles.item}>
-        <img src={languages[0].imgUrl} />
-        <p>{languages[0].name}</p>
-      </li>
-      <li className={styles.item}>
-        <img src={languages[1].imgUrl} />
-        <p>{languages[1].name}</p>
-      </li>
-      <li className={styles.item}>
-        <img src={languages[2].imgUrl} />
-        <p>{languages[2].name}</p>
-      </li>
+     {
+        languages.map((languages) => (
+          <li key={languages.id}>
+            <img
+            src ={languages.imgUrl} />
+            <p>{languages.name}</p>
+          </li>
+        ))
+      }
     </ul>
   )
 }
