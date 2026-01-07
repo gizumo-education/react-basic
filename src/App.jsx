3d58bf2c332276e7f92bc3a14e3387b6a9c1dde5
海老名彩希 <ebinamasato@MacBook-Air.local>
2026-01-07 11:02:10 +0900

    section10

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Card } from './components/Card'
 import { TodoItem } from './components/TodoItem'
 import { LanguageList } from './components/LanguageList'
 import { AlertButton } from './components/AlertButton'
+import { InputField } from './components/InputField'
 
 // import React from 'react'
 
@@ -49,6 +50,10 @@ export const App = () => {
       <hr />
 
       <AlertButton></AlertButton>
+
+      <hr />
+
+      <InputField></InputField>
 
     </div>
   )
