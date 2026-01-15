9912d728fcea5c2160c12574004fee17f9feac72
shinmoto <shinmoto@gizumo-admin-44noMacBook-Air.local>
2026-01-15 15:01:35 +0900

    Profileコンポーネントを配置

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 import { Button } from './components/Button'
+import { Profile } from './components/Profile'
 
 export const App = () => {
   return (
@@ -11,6 +12,8 @@ export const App = () => {
       {/* 以下に練習問題の回答を記述してください */}
       <hr />
       <Button /><Button /><Button />
+      <hr />
+      <Profile />
     </div>
   )
 }
