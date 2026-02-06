e31d75faf3c97003abd407b7d110367ac7b41cc0
taikifujino0511 <taiki.fujino@giztech-pro.com>
2026-02-06 18:51:15 +0900

    Add Section 8 リストレンダー

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Profile } from './components/Profile'
 import { Country } from './components/Country'
 import { Card } from './components/Card'
 import { TodoItem } from './components/TodoItem'
+import { LanguageList } from './components/LanguageList'
 
 export const App = () => {
   return (
@@ -35,12 +36,16 @@ export const App = () => {
       </Card>
 
       <hr />
-      { /*Section7*/ }
+      { /* Section7 */ }
       <ul>
         <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
         <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
         <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
       </ul>
+
+      <hr />
+      { /* Section8 */ }
+      <LanguageList />
     </div>
   )
 }
