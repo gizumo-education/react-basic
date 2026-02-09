7150623c876ec8196d34040130fd82ae530843a3
taikifujino0511 <taiki.fujino@giztech-pro.com>
2026-02-09 12:35:25 +0900

    Fix Buttonコンポーネントをnamed exportに変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 
-import Button from './components/Button/index'
+import { Button } from './components/Button/index'
 import { Profile } from './components/Profile'
 import { Country } from './components/Country'
 import { Card } from './components/Card'
