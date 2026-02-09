c83b921e153b82fa43a05c58f042090de69d5e94
taikifujino0511 <taiki.fujino@giztech-pro.com>
2026-02-09 10:28:35 +0900

    Add Section9 イベントハンドラ

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Country } from './components/Country'
 import { Card } from './components/Card'
 import { TodoItem } from './components/TodoItem'
 import { LanguageList } from './components/LanguageList'
+import { AlertButton } from './components/AlertButton'
 
 export const App = () => {
   return (
@@ -46,6 +47,10 @@ export const App = () => {
       <hr />
       { /* Section8 */ }
       <LanguageList />
+
+      <hr />
+      { /* Section9 */ }
+      <AlertButton />
     </div>
   )
 }
