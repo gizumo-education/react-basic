b9849039e0a0698d84de34cab419974f8687cb91
加藤花実 <hanami@katoukajitsunoMacBook-Air.local>
2026-03-02 18:08:55 +0900

    Section10 フックとuseState 問題1の回答を追加

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Card } from './components/Card/index.jsx'
 import { TodoItem } from './components/TodoItem/index.jsx'
 import { LanguageList } from './components/LanguageList/index.jsx'
 import { AlertButton } from './components/AlertButton/index.jsx'
+import { InputField } from './components/InputField/index.jsx'
 
 export const App = () => {
   return (
@@ -54,7 +55,11 @@ export const App = () => {
 
       <hr />
 
-          <AlertButton />
+      <AlertButton />
+
+      <hr />
+
+      <InputField />
 
     </div>
   )
