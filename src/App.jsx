872b9e6d84f3cd6696555c56543132bcaaf735fe
加藤花実 <hanami@katoukajitsunoMacBook-Air.local>
2026-03-02 14:36:00 +0900

    Section7 条件付きレンダー 問題1の回答を追加

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Button } from './components/Button/index.jsx'
 import { Profile } from './components/Profile/index.jsx'
 import { Country } from './components/Country/index.jsx'
 import { Card } from './components/Card/index.jsx'
+import { TodoItem } from './components/TodoItem/index.jsx'
 
 export const App = () => {
   return (
@@ -14,28 +15,37 @@ export const App = () => {
       {/* 以下に練習問題の回答を記述してください */}
       <hr />
 
-      
-        <Button />
-        <Button />
-        <Button />
-      
+
+      <Button />
+      <Button />
+      <Button />
+
 
       <hr />
 
       <Profile />
 
       <hr />
-    
-    <Card>
-      <Country
-        info={{
-          imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
-          name: 'アメリカ'
-        }}
-        width={120}
-        height={80}
-      />
+
+      <Card>
+        <Country
+          info={{
+            imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+            name: 'アメリカ'
+          }}
+          width={120}
+          height={80}
+        />
       </Card>
+
+      <hr />
+
+      <ul>
+        <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
+        <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
+        <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
+      </ul>
+
     </div>
   )
 }
--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -2,9 +2,16 @@ import PropTypes from 'prop-types'
 import styles from './index.module.css'
 
 export const TodoItem = ({ name, isToday, isCompleted }) => {
-  return <li className={styles.name}>{name}</li>
+  if (isToday) {
+    return (
+      <li className={styles.name}>
+        {name}
+        {isCompleted && '✔️'}
+      </li>
+    )
+  }
+  return null
 }
-
 TodoItem.propTypes = {
   name: PropTypes.string.isRequired,
   isToday: PropTypes.bool.isRequired,
