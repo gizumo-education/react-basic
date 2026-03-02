ca41e1005767835844d5043afb93b5af50410c38
加藤花実 <hanami@katoukajitsunoMacBook-Air.local>
2026-03-02 18:29:03 +0900

    Section11 問題1の回答を追加

--- src/components/CharacterImage/index.jsx
+++ src/components/CharacterImage/index.jsx
@@ -1,13 +1,16 @@
 import axios from 'axios'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export const CharacterImage = () => {
   const [imgUrl, setImgUrl] = useState('')
 
+  useEffect(()=>{
+ 
   axios.get('https://pokeapi.co/api/v2/pokemon/1').then((res) => {
+    console.log(res.data.sprites.other['official-artwork']['front_default'])
     setImgUrl(res.data.sprites.other['official-artwork']['front_default'])
   })
-
+ },[])
   return (
     <>
       {imgUrl && <img src={imgUrl} width={100} height={100} />}
