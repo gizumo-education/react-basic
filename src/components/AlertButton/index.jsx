eec66cb711166dfa76215a5063b0c20f56e8f6c2
ren <sk93txnm1582@gmail.com>
2026-03-23 21:32:50 +0900

    section9

--- src/components/AlertButton/index.jsx
+++ src/components/AlertButton/index.jsx
@@ -1,5 +1,13 @@
 import styles from './index.module.css'
 
 export const AlertButton = () => {
-  return <button className={styles.button}>Alert</button>
+  const handleAlert = () => {
+    alert('エラーが発生しました')
+  }
+
+  return (
+    <button className={styles.button} onClick={handleAlert}>
+      Alert
+    </button>
+  )
 }
