59bb794b1b57b11dd7164a7593ae985f85fcec53
ren <sk93txnm1582@gmail.com>
2026-03-23 15:10:04 +0900

    section4

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
+import { Button } from './components/Button'
 
 export const App = () => (
-    <div>
-      <img src={reactLogo} className={styles['react-logo']} />
-      <h1>Hello React!</h1>
+  <div>
+    <img src={reactLogo} className={styles['react-logo']} />
+    <h1>Hello React!</h1>
 
-      {/* 以下に練習問題の回答を記述してください */}
-      <p>component_practice</p>
-    </div>
-  );
+    {/* 以下に練習問題の回答を記述してください */}
+    <p>component_practice</p>
+
+    <hr />
+    {/* Button コンポーネントを 3 つ表示 */}
+    <Button />
+    <Button />
+    <Button />
+  </div>
+);
+
+
 
 
 
