98f4ca51af2ed8ffca2605a872f20eb2bbc8ff15
ren <sk93txnm1582@gmail.com>
2026-03-23 17:37:19 +0900

    section7

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Button } from './components/Button'
 import { Profile } from './components/Profile'
 import { Country } from './components/Country'
 import { Card } from './components/Card'
+import { TodoItem } from './components/TodoItem'
 
 export const App = () => (
   <div>
@@ -35,5 +36,14 @@ export const App = () => (
         height={80}
       />
     </Card>
+
+    <hr />
+    {/* <!-- ulタグとTodoItemコンポーネントのタグをここに記述 --> */}
+    <ul>
+      <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
+      <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
+      <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
+    </ul>
+
   </div>
 );
