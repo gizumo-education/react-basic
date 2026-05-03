9119fe7d20d2b99aa6d1a6b631a24a7cc5a10b46
3tsuya <mi3ider@gmail.com>
2026-05-04 06:31:57 +0900

    Section6 props

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 import { Button } from './components/Button'
 import { Profile } from './components/Profile'
+import { Card } from './components/Card' 
+import { Country } from './components/Country'
 
 export const App = () => {
   return (
@@ -20,6 +22,11 @@ export const App = () => {
     {/*!-- Profileコンポーネントのタグをここに記述 --*/}
       <Profile />
 
+    <hr />
+    {/*!-- CardコンポーネントとCountryコンポーネントのタグをここに記述 --*/}
+      <Card>
+      <Country />
+      </Card>
      </div>
   )
 }
--- src/components/Country/index.jsx
+++ src/components/Country/index.jsx
@@ -1,10 +1,33 @@
 import styles from './index.module.css'
+import PropTypes from 'prop-types'
 
-export const Country = () => {
+export const Country = ({ info, width, height }) => {
   return (
     <div>
-      <img src={info.imgUrl} width={width} height={height} />
+      <img
+        src={info.imgUrl}
+        width={width}
+        height={height}
+      />
       <p className={styles.name}>{info.name}</p>
     </div>
   )
+}
+
+Country.propTypes = {
+  info: PropTypes.exact({
+    imgUrl: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+  width: PropTypes.number,
+  height: PropTypes.number,
+}
+
+Country.defaultProps = {
+  info : {
+  imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+  name: 'アメリカ'
+  },
+  width: 120,
+  height: 80,
 }
