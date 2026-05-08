0a016cf2cb4bed89cae8018270fa6a8ca11ce3da
takeshi <takeshi.yamada1010@gmail.com>
2026-05-08 15:21:49 +0900

    section5

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 import { Button } from './components/Button/index'
+import { Profile } from './components/Profile/index'
 
 export const App = () => {
   return (
     <div>
-    <img src={reactLogo} className={styles['react-logo']} />
-    <h1>Hello React!</h1>
+      <img src={reactLogo} className={styles['react-logo']} />
+      <h1>Hello React!</h1>
 
-    {/* 以下に練習問題の回答を記述してください */}
-    <hr />
-    <Button />
-    <Button />
-    <Button />
+      {/* 以下に練習問題の回答を記述してください */}
+      <hr />
+      <Button />
+      <Button />
+      <Button />
+      <hr />
+      <Profile />
     </div>
+
   )
 }
