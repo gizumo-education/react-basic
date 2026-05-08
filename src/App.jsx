95fa307a8157639b184eea9568b937659aba1bc4
takeshi <takeshi.yamada1010@gmail.com>
2026-05-08 17:59:34 +0900

    section8

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Profile } from './components/Profile/index'
 import { Country } from './components/Country'
 import { Card } from './components/Card'
 import { TodoItem } from './components/TodoItem'
+import { LanguageList } from './components/LanguageList'
 
 export const App = () => {
   return (
@@ -40,6 +41,8 @@ export const App = () => {
         <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
         <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
       </ul>
+      <hr />
+      <LanguageList />
     </div>
 
   )
