3e80813614e0278e6322c2bf55f3b694dfadcb12
takeshi <takeshi.yamada1010@gmail.com>
2026-05-08 17:50:35 +0900

    Countryの軽微な修正

--- src/components/Country/index.jsx
+++ src/components/Country/index.jsx
@@ -10,7 +10,7 @@ export const Country = ({info, width, height}) => {
   )
 }
 
-Country.PropTypes = {
+Country.propTypes = {
   info: PropTypes.exact({
     imgUrl: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired
