faf7cd740601ff3d9c896fd6c8c72023d2595963
KaiSaxophone <kai.saxophone@gmail.com>
2026-06-16 11:44:38 +0900

    add: Section7 TodoItemのレンダー実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import reactLogo from './assets/react.svg'
-import styles from './App.module.css'
-import {Button} from './components/Button'
-import { Profile } from './components/Profile'
-import { Country } from './components/Country'
-import { Card } from './components/Card'
+import reactLogo from "./assets/react.svg";
+import styles from "./App.module.css";
+import { Button } from "./components/Button";
+import { Profile } from "./components/Profile";
+import { Country } from "./components/Country";
+import { Card } from "./components/Card";
+import { TodoItem } from "./components/TodoItem";
 
 export const App = () => {
   return (
     <div>
-      <img src={reactLogo} className={styles['react-logo']} />
+      <img src={reactLogo} className={styles["react-logo"]} />
       <h1>Hello React!</h1>
 
       {/* 以下に練習問題の回答を記述してください */}
-      
+
       {/* Section 4 */}
       <hr />
       <Button />
@@ -24,16 +25,23 @@ export const App = () => {
       <Profile />
 
       {/* Section 6 */}
-        <hr />
-        <Card>
-          <Country
-            info = {{
-              imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
-              name: 'アメリカ'
-            }}
-          />
-        </Card>
+      <hr />
+      <Card>
+        <Country
+          info={{
+            imgUrl: "https://www.civillink.net/sozai/kokki100/pics2654.gif",
+            name: "アメリカ",
+          }}
+        />
+      </Card>
 
+      {/* Section 7 */}
+      <hr />
+      <ul>
+        <TodoItem name="Reactの学習" isToday={true} isCompleted={true} />
+        <TodoItem name="HTMLの復習" isToday={true} isCompleted={false} />
+        <TodoItem name="React Hooksの予習" isToday={false} isCompleted={false} />
+      </ul>
     </div>
-  )
-}
+  );
+};
