7079c863af3407eaa24d50c9493e1c1c04c59fac
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-07-16 17:59:22 +0900

    Cardコンポーネントを追加してCountryを表示

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import reactLogo from './assets/react.svg'
-import styles from './App.module.css'
-import { Button } from './components/Button'
-import { Profile } from './components/Profile'
+import reactLogo from "./assets/react.svg";
+import styles from "./App.module.css";
+import { Button } from "./components/Button";
+import { Profile } from "./components/Profile";
+import { Country } from "./components/Country";
+import { Card } from "./components/Card";
 
 export const App = () => {
   return (
     <div>
-      <img src={reactLogo} className={styles['react-logo']} />
+      <img src={reactLogo} className={styles["react-logo"]} />
       <h1>Hello React!</h1>
 
       {/* 以下に練習問題の回答を記述してください */}
@@ -15,7 +17,18 @@ export const App = () => {
       <Button />
       <Button />
       <hr />
-      <Profile/>
+      <Profile />
+      <hr />
+      <Card>
+        <Country
+          info={{
+            imgUrl: "https://www.civillink.net/sozai/kokki100/pics2654.gif",
+            name: "アメリカ",
+          }}
+          width={120}
+          height={80}
+        />
+      </Card>
     </div>
-  )
-}
+  );
+};
--- src/components/Country/index.jsx
+++ src/components/Country/index.jsx
@@ -1,10 +1,20 @@
-import styles from './index.module.css'
+import styles from "./index.module.css";
+import PropTypes from "prop-types";
 
-export const Country = () => {
+export const Country = ({ info, width = 120, height = 80 }) => {
   return (
     <div>
       <img src={info.imgUrl} width={width} height={height} />
       <p className={styles.name}>{info.name}</p>
     </div>
-  )
-}
+  );
+};
+
+Country.propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+  info: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgUrl: PropTypes.string.isRequired,
+  }).isRequired,
+};
