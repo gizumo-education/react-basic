961ee92415a0cb29696e3d371c17fda687f4fca1
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-07-17 15:05:29 +0900

    LanguageListコンポーネントをmap()でリストレンダリングするよう修正

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Profile } from "./components/Profile";
 import { Country } from "./components/Country";
 import { Card } from "./components/Card";
 import { TodoItem } from "./components/TodoItem";
+import { LanguageList } from "./components/LanguageList";
 
 export const App = () => {
   return (
@@ -40,6 +41,8 @@ export const App = () => {
           isCompleted={false}
         />
       </ul>
+      <hr />
+      <LanguageList />
     </div>
   );
 };
--- src/components/LanguageList/index.jsx
+++ src/components/LanguageList/index.jsx
@@ -1,26 +1,32 @@
-import styles from './index.module.css'
+import styles from "./index.module.css";
 
 export const LanguageList = () => {
   const languages = [
-    { id: 1, name: 'HTML', imgUrl: 'https://img.icons8.com/color/48/html-5--v1.png' },
-    { id: 2, name: 'JavaScript', imgUrl: 'https://img.icons8.com/color/48/javascript--v1.png' },
-    { id: 3, name: 'TypeScript', imgUrl: 'https://img.icons8.com/color/48/typescript.png' },
-  ]
+    {
+      id: 1,
+      name: "HTML",
+      imgUrl: "https://img.icons8.com/color/48/html-5--v1.png",
+    },
+    {
+      id: 2,
+      name: "JavaScript",
+      imgUrl: "https://img.icons8.com/color/48/javascript--v1.png",
+    },
+    {
+      id: 3,
+      name: "TypeScript",
+      imgUrl: "https://img.icons8.com/color/48/typescript.png",
+    },
+  ];
 
   return (
     <ul className={styles.wrapper}>
-      <li className={styles.item}>
-        <img src={languages[0].imgUrl} />
-        <p>{languages[0].name}</p>
-      </li>
-      <li className={styles.item}>
-        <img src={languages[1].imgUrl} />
-        <p>{languages[1].name}</p>
-      </li>
-      <li className={styles.item}>
-        <img src={languages[2].imgUrl} />
-        <p>{languages[2].name}</p>
-      </li>
+      {languages.map((language) => (
+        <li key={language.id} className={styles.item}>
+          <img src={language.imgUrl} />
+          <p>{language.name}</p>
+        </li>
+      ))}
     </ul>
-  )
-}
+  );
+};
