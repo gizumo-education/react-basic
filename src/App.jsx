265d2e9a0c7fa78c0eac95060610916e9d3fee02
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-07-17 12:56:29 +0900

    TodoItemコンポーネントを追加し、条件付きレンダーで今日のToDoを表示

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Button } from "./components/Button";
 import { Profile } from "./components/Profile";
 import { Country } from "./components/Country";
 import { Card } from "./components/Card";
+import { TodoItem } from "./components/TodoItem";
 
 export const App = () => {
   return (
@@ -29,6 +30,16 @@ export const App = () => {
           height={80}
         />
       </Card>
+      <hr />
+      <ul>
+        <TodoItem name="Reactの学習" isToday={true} isCompleted={true} />
+        <TodoItem name="HTMLの復習" isToday={true} isCompleted={false} />
+        <TodoItem
+          name="React Hooksの予習"
+          isToday={false}
+          isCompleted={false}
+        />
+      </ul>
     </div>
   );
 };
