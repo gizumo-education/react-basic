f617d65c4a89c669e9928d88022ed88eb3700ddf
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-07-21 15:57:10 +0900

    refactor: AlertButtonのイベントハンドラをhandleAlertに切り出す

--- src/components/AlertButton/index.jsx
+++ src/components/AlertButton/index.jsx
@@ -1,11 +1,24 @@
+// import styles from "./index.module.css";
+
+// export const AlertButton = () => {
+//   return (
+//     <button
+//       className={styles.button}
+//       onClick={() => alert("Alertボタンが押されました")}
+//     >
+//       Alert
+//     </button>
+//   );
+// };
+
 import styles from "./index.module.css";
 
 export const AlertButton = () => {
+  const handleAlert = () => {
+    alert("Alertボタンが押されました");
+  };
   return (
-    <button
-      className={styles.button}
-      onClick={() => alert("Alertボタンが押されました")}
-    >
+    <button className={styles.button} onClick={handleAlert}>
       Alert
     </button>
   );
