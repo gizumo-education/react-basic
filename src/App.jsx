e762ecd80d73e36b48ab808acedba013b53443ab
kimuraAyuri <ayurin533@icloud.com>
2026-07-23 20:16:12 +0900

    React Lesson セクション４の練習問題

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
+import Botton from './components/Button'
 export const App = () => {
     return (
         <div>
@@ -7,6 +8,12 @@ export const App = () => {
             <h1>Hello React!</h1>
 
             {/* 以下に練習問題の回答を記述してください */}
+            <hr />
+            <div>
+                <Botton />
+                <Botton />
+                <Botton />
+            </div>
         </div>
     )
 }
