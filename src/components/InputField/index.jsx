57bce1435d142e583f0a84786f45469bb2e2be6c
Sora Shimizu <s.sky.0928@i.softbank.jp>
2026-09-20 23:36:27 +0900

    section10 commit

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,11 +1,20 @@
 import styles from './index.module.css'
+import { useState } from 'react'
 
 export const InputField = () => {
-  let value = ''
+  const [value, setValue] = useState('');
+  const hanldeInputChange = (e) => {
+    return setValue(e.target.value);
+  }
 
   return (
     <>
-      <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
+      <input
+        onChange={hanldeInputChange}
+        className={styles.input}
+        placeholder='任意の文字を入力'
+        value={ value }
+      />
       <p>入力文字：{value}</p>
     </>
   )
