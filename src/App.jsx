839d953ff3dfae902ec57cd2ab1d8b81cef1fa3c
北田なつき <kitadanatsuki@natsukimac.local>
2026-09-24 17:16:39 +0900

    Section6 練習問題を記述

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
-import { Button} from './components/Button'
+import { Button } from './components/Button'
 import { Profile } from './components/Profile'
+import { Country } from './components/Country'
+import { Card } from './components/Card'
+
 
 export const App = () => {
-  return(
+  return (
     <div>
       <img src={reactLogo} className={styles['react-logo']} />
       <h1>Hello React!</h1>
@@ -21,6 +24,11 @@ export const App = () => {
       {/* <!-- Profileコンポーネントのタグをここに記述 --> */}
       <Profile />
 
+      <hr />
+      {/* <!-- CardコンポーネントとCountryコンポーネントのタグをここに記述 --> */}
+      <Card>
+      <Country info={{ imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif', name: 'アメリカ' }} />
+      </Card>
 
 
     </div>
