629c9bbb1d94ba0ad8e1fcba8c261322d184b324
北田なつき <kitadanatsuki@natsukimac.local>
2026-09-25 10:34:30 +0900

    Section9 練習問題を記述

--- src/components/AlertButton/index.jsx
+++ src/components/AlertButton/index.jsx
@@ -1,5 +1,9 @@
 import styles from './index.module.css'
 
 export const AlertButton = () => {
-  return <button className={styles.button}>Alert</button>
+  const handleAlert = () => {
+    alert('really?')
+  }
+
+  return <button className={styles.button} onClick={handleAlert}>Alert</button>
 }
