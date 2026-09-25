9768390f33aa4fa18327d8ab5da663bb8943147d
北田なつき <kitadanatsuki@natsukimac.local>
2026-09-25 11:38:44 +0900

    Section11 練習問題を記述

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { TodoItem } from './components/TodoItem'
 import { LanguageList } from './components/LanguageList'
 import { AlertButton } from './components/AlertButton'
 import { InputField } from './components/InputField'
+import { CharacterImage } from './components/CharacterImage'
 
 
 export const App = () => {
@@ -55,6 +56,8 @@ export const App = () => {
       <hr />
       <InputField />
 
+      <hr />
+      <CharacterImage />
     </div>
   )
 }
